a0684d7b48f5ba7841f1023625465c107d00d8cb
masahiro-ai-cz <71645594+masahiro-ai-cz@users.noreply.github.com>
2021-07-24 18:22:04 +0900

    Add files via upload

---
+++ matplot.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import rospy
+from geometry_msgs.msg import Twist
+
+data01 = np.loadtxt("./data01.txt")
+fig = plt.figure(figsize = (6,6))
+ax = fig.add_subplot(111)
+#ax = plt.subplots()
+#fig, ax = plt.subplots()
+
+grid_num = 3
+resolution = 1
+
+#ロボットによる移動・回転
+
+
+#x,y座標の移動量a,b
+a = 1
+b = 1
+#theta = np.pi * 1/4
+#a = 0
+#b = 0
+theta = 0
+#ロボットの初期座標(0~grid_num)
+x_r = 1
+y_r = 1
+
+#0,1のindexの取り出し
+([x0,y0]) = np.where(data01 == 0 )
+#([x1,y1]) = np.where(data01 == 1)
+
+#print(len(x0))
+#print(x0,y0)
+
+#affine transformation
+x00 =  x0*np.cos(theta) - y0*np.sin(theta) + a
+y00 =  x0*np.sin(theta) + y0*np.cos(theta) + b
+#[x1] =  [x1*math.cos(theta) - y1*math.sin(theta) + a]
+#[y1] =  [x1*math.sin(theta) + y1*math.cos(theta) + b]
+
+print(x00,y00)
+
+ax.set_xlim(-3, 3)
+ax.set_ylim(-3, 3)
+
+def real2grid_index_fixed_grid_num(x1, y1, resolution):
+    return np.floor(x1/resolution).astype(int),\
+           np.floor(y1/resolution).astype(int)
+
+obs_map = np.zeros([grid_num, grid_num])
+
+print(len(x00))
+
+#0,1のindexの位置にそれぞれ白黒の割当を行う
+for i in range(len(x00)):
+    x1 = x00[i]
+    y1 = y00[i]
+#    print(real2grid_index_fixed_grid_num(x1, y1, resolution))
+    print(x1,y1)
+    x_idx, y_idx = real2grid_index_fixed_grid_num(x1, y1, resolution)
+#    print(x_idx,y_idx)
+    obs_map[grid_num-y_idx][x_idx] = 1
+
+#for x0,y0,x1,y1 in zip([x0],[y0],[x1],[y1]):
+#        ax.scatter(x0+0.5, y0+0.5, s = 12136, marker = "s", color="black")
+#        ax.scatter(x1+0.5, y1+0.5, s = 12136, marker = "s", color="blue")
+
+plt.imshow(obs_map)
+plt.show()
---
+++ test.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import rospy
+from geometry_msgs.msg import Twist
+
+linear_x = 0
+angular_z = 0
+A = 0
+B = 0
+
+def callback(vel_msg):
+    rospy.loginfo("Liner:%f",vel_msg.linear.x)
+    rospy.loginfo("angular:%f",vel_msg.angular.z)
+    global linear_x , angular_z
+    linear_x = vel_msg.linear.x
+    angular_z = vel_msg.angular.z
+
+    #信号受信間隔
+#    t = 1
+#    global A ,B
+    #1回の信号での変位
+#    A = linear_x * t
+#    B = angular_z * t
+#    print(A,B)
+
+def xy_r():
+    def callback(vel_msg):
+        rospy.loginfo("Liner:%f",vel_msg.linear.x)
+        rospy.loginfo("angular:%f",vel_msg.angular.z)
+        global linear_x , angular_z
+        linear_x = vel_msg.linear.x
+        angular_z = vel_msg.angular.z
+        return linear_x , angular_z
+#print(xy_r())
+
+#信号受信間隔
+#t = 1
+#1回の信号での変位
+#A = linear_x * t
+#B = angular_z * t
+    #環境の変位
+#    x_d = (x0-x_r)*np.cos(B) + (y0-y_r)*np.sin(B) - A*np.cos(B)
+#    y_d = -(x0-x_r)*np.sin(B) + (y0-y_r)*np.cos(B) - A*np.sin(B)
+
+#    print(vel_msg.angular.z * 180/3.14159236)
+#print(A,B)
+
+def listener():
+    rospy.init_node('robot_cleaner' , anonymous=True)
+
+    rospy.Subscriber("/turtle1/cmd_vel",Twist,callback)
+
+    rospy.spin()
+
+if __name__=='__main__':
+    listener()
+
+
+
+#a = np.loadtxt("./data01.txt")
+#if a == 0:
+#    x =
+#    y =
+#print(x)
+
+
+#x=[1,2,3,1,2,3,1,2,3]
+#y=[1,2,3,1,2,3,1,2,3]
+
+#for i in range(len(x)):
+#    x1=x[i]
+#    y1=y[i]
+#    print(x1,y1)
+
+#x = [12,3,24,45,56]
+#y = [56,7,33,78,32]
+#print(x,y)
+#for k in range(5):
+#    print(x[k],y[k])
+
+#for i in range (grid_num * grid_num):
+#    (x0(i),y0(i)) = np.where(a == 0)
+#    print(x0(i),y0(i))
+#for x0 in
+#    print(x0,y0)
+
+#for x in range(9):
+#    np.where(a == 1)
+#    print(x)
+
+# FigureとAxesを作成
+#fig = plt.figure(figsize = (6, 6))
+#ax = fig.add_subplot(111)
+
+# 軸範囲を設定
+#ax.set_xlim(0, 10)
+#ax.set_ylim(0, 10)
+
+# 3≦x≦6の範囲を塗り潰す
+#ax.axvspan(0, 0, color = "coral")
+
+
+
+#plt.show()
